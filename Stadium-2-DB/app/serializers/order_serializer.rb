023fe2206fd5bd66c6ce7d14d3733e4c77d709ed
Joseph Hirotsu <jhirotsu@gmail.com>
2022-09-07 02:23:49 -0400

    removed db2

--- Stadium-2-DB/app/serializers/order_serializer.rb
+++
@@ -1,3 +0,0 @@
-class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :product_id, :sales_tax, :fees, :tips, :total_price, :order_number
-end
--- Stadium-2-DB/app/serializers/product_serializer.rb
+++
@@ -1,3 +0,0 @@
-class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :category, :product_name, :product_image_url, :description, :price
-end
